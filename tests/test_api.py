938fa94a359d2a204cd41f504f6ebd320ab3f540
Jonathan <jonathansouzasilva@hotmail.com>
2022-01-22 17:10:39 -0300

    Initial project structure

---
+++ tests/test_api.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class ApiTestCase(unittest.TestCase):
+    def test_success(self):
+        pass
+
+    def test_without_clinics(self):
+        pass
+
+    def test_rollback(self):
+        pass
+
+    def test_error(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_iclinic_api.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class IClinicApiTestCase(unittest.TestCase):
+    def test_success_response(self):
+        pass
+
+    def test_malformed_request(self):
+        pass
+
+    def test_not_found(self):
+        pass
+
+    def test_not_available(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
